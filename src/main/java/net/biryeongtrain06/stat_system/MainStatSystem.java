1e220c20f8537148ea0b6c57a1fdd540cefd9df5
biryeongtrain <biryeongtrain06@gmail.com>
2022-08-11 16:46:46 +0900

    backup

--- src/main/java/net/biryeongtrain06/stat_system/MainStatSystem.java
+++ src/main/java/net/biryeongtrain06/stat_system/MainStatSystem.java
@@ -1,24 +1,27 @@
 package net.biryeongtrain06.stat_system;
 
-import net.biryeongtrain06.stat_system.testComponent.TestComponent;
+import net.biryeongtrain06.stat_system.sidebar.OpenDebugBar;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.minecraft.util.ActionResult;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
 
 public class MainStatSystem implements ModInitializer {
     public static String MOD_ID ="cardinal";
     final public static Logger debugLogger = LogManager.getLogger("Qf Stat Debug");
 
+
     @Override
     public void onInitialize() {
-        AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
-
-                TestComponent.useMagik(player,entity);
-
-            return ActionResult.PASS;
-        }));
-
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(
+                    literal("opendebugbar").requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
+                            .executes(OpenDebugBar::OpenGUI)
+            );
+        });
     }
 }
+
