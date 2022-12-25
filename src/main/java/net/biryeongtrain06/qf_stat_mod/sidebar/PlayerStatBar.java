6c34b6d9dba84ba3f394b5320ae5ab2bb824fc6c
biryeongtrain <biryeongtrain06@gmail.com>
2022-12-25 23:17:04 +0900

    backup

--- src/main/java/net/biryeongtrain06/qf_stat_mod/sidebar/PlayerStatBar.java
+++ src/main/java/net/biryeongtrain06/qf_stat_mod/sidebar/PlayerStatBar.java
@@ -5,6 +5,7 @@ import eu.pb4.sidebars.api.Sidebar;
 import eu.pb4.sidebars.api.lines.SidebarLine;
 import net.biryeongtrain06.qf_stat_mod.api.DataStorage;
 import net.biryeongtrain06.qf_stat_mod.player.IServerPlayerEntity;
+import net.biryeongtrain06.qf_stat_mod.utils.PlayerHelper;
 import net.biryeongtrain06.qf_stat_mod.utils.TextHelper;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -28,16 +29,14 @@ public class PlayerStatBar {
         sidebar.addLines(SidebarLine.create(0, (p) -> Text.literal("Class : ")
                 .append(PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getPlayer_class().getClassText())));
 
-       /* sidebar.addLines(SidebarLine.create(0, (p) -> Text.literal("HP : ")
+       sidebar.addLines(SidebarLine.create(0, (p) -> Text.literal("HP : ")
                 .append(Text.literal(PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getCurrentHealth() + ""))
                 .append(Text.literal(" / " + PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getMaxHealth()))
                 .formatted(
-                        PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getCurrentHealth() / PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getMaxHealth() * 100 > 80 ?
-                                Formatting.GREEN : PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getCurrentHealth() / PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getMaxHealth() * 100 > 40 ?
-                                Formatting.GOLD : Formatting.RED
+                        PlayerHelper.getPlayerHealthFormat(p)
                 )
         ));
-        */
+
         sidebar.addLines(SidebarLine.create(0, (p) -> Text.literal("Lv : " + PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getLevel())));
         sidebar.addLines(SidebarLine.create(0, (p) -> Text.literal("XP : " + PlayerDataApi.getCustomDataFor(p, PLAYER_STAT_DATA_STORAGE).getXP())
                 .append(Text.literal( " / " + DataStorage.loadPlayerStat(p).getNeedXpToLevelUp()))));
